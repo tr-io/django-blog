3d6c80dcab89b7389db1564a5637ed6c4f355c03
tr-io <leoliu.mailbox@gmail.com>
2018-05-29 21:19:58 -0400

    New updates for Heroku deployment

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mz#s_ok9ugu(f9%xzesesyuxgl9*a(dykpxl3_%f6=g(mta1#l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
